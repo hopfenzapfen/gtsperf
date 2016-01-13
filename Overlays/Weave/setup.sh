5a422682baf775154d9cbb5a06458cea366e2766
Siem Hermans <siemhermans@users.noreply.github.com>
2016-01-13 14:21:01 +0100

    Automate Weave installation

--- Overlays/Weave/setup.sh
+++ Overlays/Weave/setup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-echo "Entered Weave script"
+# Installing Weave Net
+echo "Installing Weave Net. Please wait..."
+sudo curl -L git.io/weave -o /usr/local/bin/weave
+sudo chmod +x /usr/local/bin/weave
 
+# Starting the Weave component
+read -r -p "Is this the first Weave node in the network? [y/n] " RESPONSE
+RESPONSE=${RESPONSE,,}
+if [[ $RESPONSE =~ ^(yes|y)$ ]]; then
+  export INSTALLER_TYPE=master
+  weave launch
+  weave status
+else
+  export INSTALLER_TYPE=slave
+  read -r -p "Enter the IP address of an existing Weave router in the network: " ROUTER_IP
+  weave launch $ROUTER_IP
+  weave status
+fi
