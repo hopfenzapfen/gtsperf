115c88a86b7af0f26165a232194a96894ec9c4d2
Siem Hermans <siemhermans@users.noreply.github.com>
2016-01-20 11:45:04 +0100

    Update perf_measurement.sh

--- Container/scripts/perf_measurement.sh
+++ Container/scripts/perf_measurement.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-# Generate timestamp
-psstart=$(date "+ %m/%d/%y %H:%M:%S:%3N")
-psend=$(date "+ %m/%d/%y %H:%M:%S:%3N")
-# Run performacne measurement
-psresult=$(netperf -H 127.0.0.1 -t TCP_RR -- -O min_latency,mean_latency,p99_latency,stddev_latency | tail -n 1 | awk '{$1=$1}1' OFS=",")
-# Write log to file
-echo $psstart","$psend",AMS,LJU," $psresult
+
+if [[ $MODE =~ ^(client|CLIENT)$ ]]; then
+  # netperf measurement
+    # Generate timestamp
+    psstart=$(date "+ %m/%d/%y %H:%M:%S:%3N")
+    psend=$(date "+ %m/%d/%y %H:%M:%S:%3N")
+    # Run performance measurement
+    psresult=$(netperf -H 127.0.0.1 -t TCP_RR -- -O min_latency,mean_latency,p99_latency,stddev_latency | tail -n 1 | awk '{$1=$1}1' OFS=",")
+    # Write log to file
+    echo $psstart","$psend",AMS,LJU," $psresult
+  # iperf measurement
+    iperf -c $ADDRESS 
+else
+    iperf -s -D
+    netserver
+fi
+
+
+
