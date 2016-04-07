56052e38d87a73972ac513bf2aa3f9296661c3a6
Arne Z <10495800@student.uva.nl>
2016-04-07 22:32:32 +0200

    make bootstrap use rafaela

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,8 +21,9 @@ sed -i -e 's/sa2host/'$HOSTNAME'/' /etc/hosts
 read -r -p "Install Docker? [y/n] " RESPONSE
 RESPONSE=${RESPONSE,,}
 if [[ $RESPONSE =~ ^(yes|y)$ ]]; then
+  source /etc/lsb-release
 	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-	sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+	sudo echo "deb https://apt.dockerproject.org/repo ubuntu-$DISTRIB_CODENAME main" > /etc/apt/sources.list.d/docker.list
   printf "\nUpdating sources and installing Docker. Please wait..."
   sudo apt-get -yqq update && sudo apt-get install -yqq docker-engine
   # Ensure that Docker is running
