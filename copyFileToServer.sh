1da14e4eddc01898e0f528aa79e956f59098a9f1
Arne Z <10495800@student.uva.nl>
2016-05-29 00:02:14 +0200

    lala

--- copyFileToServer.sh
+++ copyFileToServer.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-
-
-
 if [ $# -ne 1 ]; then
-	echo "Please give a file name"
+	echo "Use script like this: $0 <path to file>"
 	exit 1
 fi
 FILENAME=$1
 
+VM1=172.16.0.13
+VM2=172.16.0.10
+VM3=172.16.0.12
+VM4=172.16.0.15
+VM5=172.16.0.11
+VM6=172.16.0.14
+
 echo "Copying '${FILENAME}' to the servers"
 
-for IP in 172.16.0.28 172.16.0.24 172.16.0.29 172.16.0.30
+for IP in $VM1 $VM2 $VM3 $VM4 $VM5 $VM6
 do
 	scp $FILENAME gts@$IP:
 done
