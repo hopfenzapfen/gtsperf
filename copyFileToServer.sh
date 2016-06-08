f03ae81dccd7c63a0dfd2385481f3bdeb3ef08e6
Arne Z <10495800@student.uva.nl>
2016-06-08 15:29:51 +0200

    added python scripts and modfied Dockerfile

--- copyFileToServer.sh
+++ copyFileToServer.sh
@@ -6,16 +6,14 @@ if [ $# -ne 1 ]; then
 fi
 FILENAME=$1
 
-VM1=172.16.0.13
-VM2=172.16.0.10
-VM3=172.16.0.12
-VM4=172.16.0.15
-VM5=172.16.0.11
-VM6=172.16.0.14
+VM1=172.16.0.25
+VM2=172.16.0.27
+VM3=172.16.0.26
+VM4=172.16.0.28
 
 echo "Copying '${FILENAME}' to the servers"
 
-for IP in $VM1 $VM2 $VM3 $VM4 $VM5 $VM6
+for IP in $VM1 $VM2 $VM3 $VM4
 do
 	scp $FILENAME gts@$IP:
 done
